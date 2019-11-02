3c0ba7b3fdd7248f2070c9ee5d8bcbd467c65da1
jaydray1 <jdreher15@gmail.com>
2019-11-02 08:43:02 -0700

    trying some things

--- RetroHeadliner.js
+++ RetroHeadliner.js
@@ -4,9 +4,14 @@ import './RetroHeadliner.css';
 export const RetroHeadliner = () => {
   const myName = 'Jon-Michael Dreher'
 return (
+  <>
     <div className="name-headliner">
-    <h1> {myName} </h1>
-  </div>
+      <h1 className="namer"> {myName} </h1>
+    </div>
+    <div className="readable-headliner">
+      <h1 className="readable-name"> {myName} </h1>
+    </div>
+  </>
   )
 }
 
