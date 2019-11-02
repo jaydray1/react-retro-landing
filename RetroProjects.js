cad95897f4b33f5e57a8818e7d31f8014796aff4
jaydray1 <jdreher15@gmail.com>
2019-11-01 22:43:14 -0700

    css table to center headers and purple rain

--- RetroProjects.js
+++ RetroProjects.js
@@ -7,14 +7,22 @@ export const RetroProjects = () => {
       <h1 className="projects-header">Projects</h1>
       <div className="pro-stamp">
           <a href="https://youtu.be/QZPfwbL5fHs" target="_blank">
-            <div className="link-container"></div>
+            <div className="link-container">
+              <h2 className="link-title">Virtual Library</h2>
+            </div>
           </a>
       </div>
       <div className="pro-stamp">
-        <div className="link-container"></div>
+      <a href="https://cudemo.goalexandria.com/offline" target="_blank">
+        <div className="link-container">
+          <h2 className="link-title">Offline Data Entry</h2>
+        </div>
+        </a>
       </div>
       <div className="pro-stamp">
-        <div className="link-container"></div>
+        <div className="link-container">
+          <h2 className="link-title">Outdoor Gear</h2>
+        </div>
       </div>
   </div>
   )
